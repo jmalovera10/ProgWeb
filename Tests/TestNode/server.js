c2099dc85c64c3bc65d478c7c138a9b3c7638b24
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-02-19 13:48:13 -0500

    Function scope

--- Tests/TestNode/server.js
+++ Tests/TestNode/server.js
@@ -1,23 +1,24 @@
+'use strict';
 const express = require("express");
 
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('tweets.sqlite3');
 
-
+function getAllTweets(res){
+    db = new sqlite3.Database('tweets.sqlite3');
+    db.all("SELECT * from tweets", (err, tweets) => {
+        if (err) throw err;
+        console.log(tweets);
+        res.send(tweets);
+        db.close();
+    });
+}
 
 app.get("/", (req, res) => {
     console.log("Get /");
     //res.send("Hola bonito!");
-    db = new sqlite3.Database('tweets.sqlite3');
-    var tw = db.all("SELECT * from tweets", (err, tweets) => {
-        if (err) throw err;
-
-        console.log(tweets);
-        //db.close();
-        res.send(tweets);
-    })
-
+    getAllTweets(res);
 });
 
 app.get("/about", (req, res) => {
